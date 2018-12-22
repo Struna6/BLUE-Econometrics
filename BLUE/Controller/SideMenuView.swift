848c169ff56168abd6ea3486d3fc73c454ed3ed8
Karol Struniawski <struniawski.karol@gmail.com>
2018-12-22 15:10:38 +0100

    prepare for segue

--- BLUE/Controller/SideMenuView.swift
+++ BLUE/Controller/SideMenuView.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class SideMenuView: UITableViewController{
     
-    let sections = ["Observations", "Plots", "Tests"]
-    let options = ["Observations":["All","Selected"], "Plots":[], "Tests":[]]
+    var model = Model()
+    var identities = ["toObservations","toPlots"]
+    let sections = ["Observations", "Plots"]
+    let options = ["Observations":["All","Selected"], "Plots":["X-Y plot"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +34,22 @@ class SideMenuView: UITableViewController{
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "cellID")! as UITableViewCell
         cell.textLabel?.text = options[sections[indexPath.section]]![indexPath.row]
-        //cell.detailTextLabel?.text = options[sections[indexPath.section]]![indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let num = Int("\(indexPath.section)\(indexPath.row)")
+        switch num {
+            //All observations
+        case 00:
+            <#code#>
+            //Selected observations
+        case 01:
+            <#code#>
+            //
+        case 10:
+            <#code#>
+        default: break
+        }
     }
 }
