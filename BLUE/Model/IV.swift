bd83d52bfa0f6f6cb3ab0f7037f3db36e43da768
Karol Struniawski <struniawski.karol@gmail.com>
2019-02-11 20:16:06 +0100

    instrumental method algoriyhms 1

--- BLUE/Model/IV.swift
+++ BLUE/Model/IV.swift
@@ -11,10 +11,54 @@ import Accelerate
 import Surge
 
 protocol IVCalculable{
+    func SRIV(Z: [[Double]]) -> [Double]
+    func SSRIV(Z: [[Double]]) -> Double
+    func seIV(Z: [[Double]]) -> Double
+    func IVestimatedY(Z: [[Double]]) -> [Double]
+    func SEBIV(Z: [[Double]]) -> [Double]
+    
     func getGIVRegressionEquation(Z : [[Double]]) -> [Double]
 }
 
 extension IVCalculable where Self==Model{
+    func SRIV(Z: [[Double]]) -> [Double]{
+        var tmp = [Double]()
+        for i in 0..<flatY.count{
+            tmp.append(pow((flatY[i]-IVestimatedY(Z: Z)[i]), 2.0))
+        }
+        return tmp
+    }
+    func SSRIV(Z: [[Double]]) -> Double{
+        return sum(SRIV(Z: Z))
+    }
+    func seIV(Z: [[Double]]) -> Double{
+            return sqrt(1.0/((Double(n)-Double(Z[0].count)-1.0))*SSRIV(Z: Z))
+    }
+    func IVestimatedY(Z: [[Double]]) -> [Double]{
+        var returnTmp = [Double]()
+        var tmpY = [[Double]]()
+        tmpY.append(getGIVRegressionEquation(Z: Z))
+        let Y = Matrix<Double>(tmpY)
+        let Z = Matrix<Double>(Z)
+        let result = mul(Z, y: Surge.transpose(Y))
+        result.forEach({ (slice) in
+            returnTmp.append(Array(slice)[0])
+        })
+        return returnTmp
+    }
+    func SEBIV(Z: [[Double]]) -> [Double]{
+        let Z0 = Matrix<Double>(Z)
+        let ZT = transpose(Z0)
+        let matrix = mul((seIV(Z:Z)*seIV(Z:Z)), x: inv(mul(ZT, y: Z0)))
+        var result = [Double]()
+        var i = 0
+        matrix.forEach { (row) in
+            result.append(Array(row)[i])
+            i = i + 1
+        }
+        return result
+    }
+    
     func getGIVRegressionEquation(Z : [[Double]]) -> [Double]{
         let X = Matrix(chosenX)
         let Z = Matrix(Z)
@@ -31,10 +75,16 @@ extension IVCalculable where Self==Model{
 }
 
 protocol IVTestable : Statisticable{
+    
     func HausmannTest(Z: [[Double]]) -> Double
+    
 }
 
+private var hValue = 0.0
+private var hTestValue = 0.0
+
 extension IVTestable where Self==Model{
+    
     func HausmannTest(Z: [[Double]]) -> Double{
         let bOLS = getOLSRegressionEquation()
         let bGIV = getGIVRegressionEquation(Z: Z)
@@ -50,7 +100,23 @@ extension IVTestable where Self==Model{
         let bG = Matrix(bGt)
         let q = add(bO, y: bG)
         
+        let sbO = Matrix([SEB])
+        let sbG = Matrix([SEBIV(Z: Z)])
+        let s = add(sbO, y: mul(-1.0, x: sbG))
         
-        return 0.0
+        let H0 = mul(mul(transpose(q), y: inv(s)),y:q)
+        var H = 0.0
+        H0.forEach { (row) in
+            H = row[0]
+        }
+        
+        if H == hValue{
+            return hTestValue
+        }else{
+            hValue = H
+            let result = chiCDF(x: H, k: Double(Z[0].count-chosenX[0].count))
+            hTestValue = result
+            return result
+        }
     }
 }
