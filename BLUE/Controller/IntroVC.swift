388594dd305f6b2975ad82ac2e046807103aa9dc
Karol Struniawski <struniawski.karol@gmail.com>
2019-02-21 16:06:47 +0100

    intro done

--- BLUE/Controller/IntroVC.swift
+++ BLUE/Controller/IntroVC.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
-import Lottie
 
 class IntroVC: UIViewController {
 
     @IBOutlet weak var appIcon: UIImageView!
-    @IBOutlet weak var animationView: UIView!
-    @IBOutlet weak var whiteView: UIView!
+
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet var labelsCollection: [UILabel]!
+    @IBOutlet weak var labels: UIStackView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainView.alpha = 0
         appIcon.showHint(text: "Thank you for downloading our application!")
+        load()
+    }
+    
+    func load(){
+        mainView.transform = CGAffineTransform(translationX: -300.0, y: 0.0)
+        UIView.animate(withDuration: 0.7) {
+            self.mainView.alpha = 1
+            self.mainView.transform = CGAffineTransform.identity
+        }
         
+        UIView.animate(withDuration: 0.4, animations:{
+            self.mainView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        }){(true) in
+            sleep(1)
+            self.dismiss()
+        }
+    }
+    
+    func dismiss(){
+        UIView.animate(withDuration: 0.4){
+            self.mainView.transform = CGAffineTransform.identity
+        }
+        UIView.animate(withDuration: 0.7, animations: {
+            self.mainView.alpha = 0.0
+            self.mainView.transform = CGAffineTransform(translationX: 300.0, y: 0.0)
+        }) { (true) in
+            self.performSegue(withIdentifier: "toFirst", sender: self)
+        }
     }
 }
